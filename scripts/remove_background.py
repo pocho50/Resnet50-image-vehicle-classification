96006d376b292e4f9d50f32d68b583e65bd256d3
raf <raf@bdcsoftware.com>
2022-06-01 09:36:20 -0700

    Init commit

---
+++ scripts/remove_background.py
@@ -0,0 +1,65 @@
+"""
+This script will be used to remove noisy background from cars images to
+improve the quality of our data and get a better model.
+The main idea is to use a vehicle detector to extract the car
+from the picture, getting rid of all the background, which may cause
+confusion to our CNN model.
+We must create a new folder to store this new dataset, following exactly the
+same directory structure with its subfolders but with new images.
+"""
+
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train your model.")
+    parser.add_argument(
+        "data_folder",
+        type=str,
+        help=(
+            "Full path to the directory having all the cars images. Already "
+            "splitted in train/test sets. E.g. "
+            "`/home/app/src/data/car_ims_v1/`."
+        ),
+    )
+    parser.add_argument(
+        "output_data_folder",
+        type=str,
+        help=(
+            "Full path to the directory in which we will store the resulting "
+            "cropped pictures. E.g. `/home/app/src/data/car_ims_v2/`."
+        ),
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main(data_folder, output_data_folder):
+    """
+    Parameters
+    ----------
+    data_folder : str
+        Full path to train/test images folder.
+
+    output_data_folder : str
+        Full path to the directory in which we will store the resulting
+        cropped images.
+    """
+    # For this function, you must:
+    #   1. Iterate over each image in `data_folder`, you can
+    #      use Python `os.walk()` or `utils.waldir()``
+    #   2. Load the image
+    #   3. Run the detector and get the vehicle coordinates, use
+    #      utils.detection.get_vehicle_coordinates() for this task
+    #   4. Extract the car from the image and store it in
+    #      `output_data_folder` with the same image name. You may also need
+    #      to create additional subfolders following the original
+    #      `data_folder` structure.
+    # TODO
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args.data_folder, args.output_data_folder)
---
+++ utils/detection.py
@@ -0,0 +1,44 @@
+
+# Load here your Detection model
+# The chosen detector model is "COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"
+# because this particular model has a good balance between accuracy and speed.
+# You can check the following Colab notebook with examples on how to run
+# Detectron2 models
+# https://colab.research.google.com/drive/16jcaJoc6bCFAQ96jDe2HwtXj7BMD_-m5.
+# Assign the loaded detection model to global variable DET_MODEL
+# TODO
+DET_MODEL = None
+
+
+def get_vehicle_coordinates(img):
+    """
+    This function will run an object detector over the the image, get
+    the vehicle position in the picture and return it.
+
+    Many things should be taken into account to make it work:
+        1. Current model being used can detect up to 80 different objects,
+           we're only looking for 'cars' or 'trucks', so you should ignore
+           other detected objects.
+        2. The object detector may find more than one vehicle in the picture,
+           you must then, choose the one with the largest area in the image.
+        3. The model can also fail and detect zero objects in the picture,
+           in that case, you should return coordinates that cover the full
+           image, i.e. [0, 0, width, height].
+        4. Coordinates values must be integers, we're making reference to
+           a position in a numpy.array, we can't use float values.
+
+    Parameters
+    ----------
+    img : numpy.ndarray
+        Image in RGB format.
+
+    Returns
+    -------
+    box_coordinates : tuple
+        Tuple having bounding box coordinates as (left, top, right, bottom).
+        Also known as (x1, y1, x2, y2).
+    """
+    # TODO
+    box_coordinates = None
+
+    return box_coordinates
